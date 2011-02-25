a63373421f716ee5dbd7ffe1d89a7a48a585982e
Nils Maier <maierman@web.de>
2011-02-25 21:16:38 +0100

    Add Denis as contributor to the source code

--- browser.js
+++ browser.js
@@ -19,6 +19,7 @@
  *
  * Contributor(s):
  *    Nils Maier <MaierMan@web.de>
+ *    Denis Jasselette
  *
  * Alternatively, the contents of this file may be used under the terms of
  * either the GNU General Public License Version 2 or later (the "GPL"), or
