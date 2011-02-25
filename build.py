35f09f3d1512032edcea31aa827a289f8ea0c1ac
Nils Maier <maierman@web.de>
2011-02-25 22:47:50 +0100

    Build with chrome.jar (inner jar) to lessen the startup penality in FX 3.x

--- build.py
+++ build.py
@@ -1,16 +1,21 @@
 import os, sys, re
 from glob import glob
+from io import BytesIO
 from zipfile import ZipFile, ZIP_STORED, ZIP_DEFLATED
 
-resources = [
+chrome = [
     "*.xul",
     "*.js",
     "*.css",
     "arrows.png",
-    "locale/*/*",
-    "icon*.png",
-    "install.rdf", "chrome.manifest",
+    "locale/*/*"
     ]
+resources = [
+    "*.jsm",
+    "icon*.png",
+    "install.rdf"
+    ]
+
 destination = "fastprevnext.xpi"
 
 def get_files(resources):
@@ -21,21 +26,44 @@ def get_files(resources):
             for g in glob(r):
                 yield g
 
+def jarify(f):
+    with open(f, "rb") as fp:
+        return "".join(map(
+            lambda x: re.sub(
+                r"^((?:content|skin|locale).*?)([\S]+)$",
+                r"\1jar:chrome.jar!/\2",
+                x
+                ),
+            fp.readlines()
+            ))
+
+def zip_files(files, zp):
+    for f in sorted(files, key=str.lower):
+        if f.endswith('.png'):
+            zp.write(f, compress_type=ZIP_STORED)
+        else:
+            zp.write(f)
+
+
 if os.path.exists(destination):
     print >>sys.stderr, destination, "is in the way"
     sys.exit(1)
 
 class ZipOutFile(ZipFile):
-    def __init__(self, zfile):
-        ZipFile.__init__(self, zfile, "w", ZIP_DEFLATED)
+    def __init__(self, zfile, zstore=ZIP_DEFLATED):
+        ZipFile.__init__(self, zfile, "w", zstore)
     def __enter__(self):
         return self
     def __exit__(self, type, value, traceback):
         self.close()
 
 with ZipOutFile(destination) as zp:
-    for f in sorted(get_files(resources), key=str.lower):
-        if f.endswith('.png'):
-            zp.write(f, compress_type=ZIP_STORED)
-        else:
-            zp.write(f)
+    with BytesIO() as jarbuffer:
+        with ZipOutFile(jarbuffer, ZIP_STORED) as jp:
+            zip_files(get_files(chrome), jp)
+        jarbuffer.flush()
+        jarbuffer.seek(0,0)
+        zp.writestr("chrome.jar", jarbuffer.read())
+
+    zip_files(get_files(resources), zp)
+    zp.writestr("chrome.manifest", jarify("chrome.manifest"))
